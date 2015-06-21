a3ce7273a41d7c12d45eb7723247ee797a414647
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-21 02:43:16 -0300

    Lista 4 Questo 4: fatorial

---
+++ Lista4/Lista4-Questao4/Lista4-Questao4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//4) Faça um algoritmo que lê um valor N inteiro e positivo e que calcula e escreve o fatorial de N (N!).
+
+namespace Lista4_Questao4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, fat;
+
+            numero = int.Parse(Console.ReadLine());
+            fat = 1;
+
+            if (numero>0)
+            {
+                //fatoral 
+                for (int i = numero; i > 0; i--)
+                {
+                    fat = i * fat;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalido: Numero negativo");
+            }
+
+            Console.WriteLine("Resultado: "+fat);
+            Console.ReadKey();
+        }
+    }
+}
