c791c17b585ab9a06cbf275467cc9203497e84b2
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-21 17:27:14 -0300

    Questao 6: Tabuada da multiplacao com os três lacos de loop

---
+++ Lista4/Lista4-Questao6/Lista4-Questao6/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//6) Elabore um algoritmo que imprima a tabuada de um número qualquer. 
+
+namespace Lista4_Questao6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero,i=0, length=10;
+
+            numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("::Tabuada da Multiplicacao::");
+            //:::FOR:::
+            for ( i = 0; i <= length; i++)
+            {
+                Console.WriteLine(numero+" X "+i+" ="+(numero*i));
+            }
+
+            Console.WriteLine("::Tabuada da Multiplicacao::");
+            //:::WHILE:::
+            i = 0;
+            while (i<=length)
+            {
+                Console.WriteLine(numero + " X " + i + " =" + (numero * i));
+                i++;
+            }
+
+            Console.WriteLine("::Tabuada da Multiplicacao::");
+            //:::DOWHILE:::
+            i = 0;
+            do{
+                Console.WriteLine(numero + " X " + i + " =" + (numero * i));
+                i++;
+            } while (i <= length) ;
+
+            //saida
+            Console.ReadKey();
+        }
+    }
+}
