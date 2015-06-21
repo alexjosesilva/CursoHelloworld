36d066bd953b720145e98ddfc2efccbbc03a1908
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-21 02:16:42 -0300

    Lista4 Questao 3: soma de numeros

---
+++ Lista4/Lista4-Questao3/Lista4-Questao3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//3) Faça um programa que leia 5 valores e no final, informe o seu somatório.
+
+namespace Lista4_Questao3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, soma=0;
+            int length = 5;
+
+            
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("Numero: ");
+                numero = int.Parse(Console.ReadLine());
+                soma = soma + numero;
+            }
+
+            Console.WriteLine("Resultado: "+soma);
+            Console.ReadKey();
+        }
+    }
+}
