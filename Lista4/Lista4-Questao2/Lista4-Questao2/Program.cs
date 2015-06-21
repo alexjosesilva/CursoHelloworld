26c03a21572a1ef73dc3953023b1715ecc561ceb
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-21 01:59:33 -0300

    Lista 4 Questao 2: numero multiplos de 5 entre 1000 e 2000

---
+++ Lista4/Lista4-Questao2/Lista4-Questao2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * 2) Faça um programa que exiba todos os números múltiplos de 5, entre 1000 e 2000
+ */
+namespace Lista4_Questao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length=2000;
+
+            Console.Write("Numeros Multiplo de 5: ");
+            for (int i = 1000; i < length; i++)
+            {
+                if (i%5==0)
+                {
+                    Console.Write(" " + i);
+                }
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
