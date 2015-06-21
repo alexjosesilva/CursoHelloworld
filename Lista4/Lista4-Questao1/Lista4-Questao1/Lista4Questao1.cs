6cc2ad573a946e4ba4665edf02c0f4b0274ba75e
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-21 01:36:16 -0300

    Primeiro Comit no repostorio....

---
+++ Lista4/Lista4-Questao1/Lista4-Questao1/Lista4Questao1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista4_Questao1
+{
+    class Lista4Questao1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lista 4-Questao 1");
+            Console.ReadKey();
+        }
+    }
+}
