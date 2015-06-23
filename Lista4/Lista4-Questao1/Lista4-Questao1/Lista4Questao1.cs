a08a8781e633386acb5de322f0ef4c6f9eaaac9b
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-22 21:03:27 -0300

    Lista 4 Questao 1: insercao dos loops....

--- Lista4/Lista4-Questao1/Lista4-Questao1/Lista4Questao1.cs
+++ Lista4/Lista4-Questao1/Lista4-Questao1/Lista4Questao1.cs
@@ -15,14 +15,41 @@ namespace Lista4_Questao1
         static void Main(string[] args)
         {
             int length = 100;
+            int i = 0;
 
-            Console.WriteLine("Numeros primos: ");
-            for (int i = 0; i < length; i++)
+            //:::FOR:::
+            Console.WriteLine("\n Numeros primos: ");
+            for (i = 0; i < length; i++)
             {
                 if (i%2!=0){
                     Console.Write(" "+i);
                 }
             }
+
+            //:::WHILE:::
+            i = 0;
+            Console.WriteLine("\n Numeros primos: ");
+            while (i<length)
+            {
+                 if (i%2!=0){
+                    Console.Write(" "+i);
+                }
+                 i++;
+            }
+
+            //:::DO WHILE:::
+            i = 0;
+            Console.WriteLine("\n Numeros primos: ");
+            do
+            {
+                if (i % 2 != 0)
+                {
+                    Console.Write(" " + i);
+                }
+                i++;
+            } while (i != length);
+            
+
 
             Console.ReadKey();
         }
