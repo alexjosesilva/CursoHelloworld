6fb9639210e1286fbc2ff8f7a6da59646d7837bd
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-22 21:17:17 -0300

    alteração da questao 2 da lista 4: inserção dos loops

--- Lista4/Lista4-Questao2/Lista4-Questao2/Lista4-Questao2.cs
+++ Lista4/Lista4-Questao2/Lista4-Questao2/Lista4-Questao2.cs
@@ -14,16 +14,44 @@ namespace Lista4_Questao2
         static void Main(string[] args)
         {
             int length=2000;
+            int i;
 
-            Console.Write("Numeros Multiplo de 5: ");
-            for (int i = 1000; i < length; i++)
+            //::FOR:::
+            Console.Write(" \n Numeros Multiplo de 5: ");
+            for (i = 1000; i < length; i++)
             {
                 if (i%5==0)
                 {
                     Console.Write(" " + i);
                 }
             }
-            
+
+
+            //:::WHILE:::
+            Console.Write("\n Numeros Multiplo de 5: ");
+            i = 0;
+            while (i<length)
+            {
+                if (i % 5 == 0)
+                {
+                    Console.Write(" " + i);
+                }
+                i++;
+            }
+
+            //::DO WHILE:::
+            Console.Write("\n Numeros Multiplo de 5: ");
+            i = 0;
+            do{
+                if (i % 5 == 0)
+                {
+                    Console.Write(" " + i);
+                }
+                i++;
+
+            } while (i != length);
+
+
             Console.ReadKey();
         }
     }
