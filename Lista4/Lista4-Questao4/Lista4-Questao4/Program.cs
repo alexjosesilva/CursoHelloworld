53a8c0cfdd06c13cb6d9ced598833e0dc49034f9
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-22 22:09:06 -0300

    alteracao da questao 4: inserindo o loop: For, While, DoWhile

--- Lista4/Lista4-Questao4/Lista4-Questao4/Program.cs
+++ Lista4/Lista4-Questao4/Lista4-Questao4/Program.cs
@@ -12,18 +12,35 @@ namespace Lista4_Questao4
     {
         static void Main(string[] args)
         {
-            int numero, fat;
+            int numero, fat, i;
 
             numero = int.Parse(Console.ReadLine());
             fat = 1;
-
+            
             if (numero>0)
             {
-                //fatoral 
-                for (int i = numero; i > 0; i--)
+                //fatoral com FOR
+                for (i = numero; i > 0; i--)
                 {
                     fat = i * fat;
                 }
+
+                //fatoral com WHILE
+                i = numero;
+                while(i>0){
+                     
+                     fat = i * fat;
+                     i--;
+                }
+
+                //fatoral com DO WHILE
+                i = numero;
+                do{
+                    
+                    fat = i * fat;
+                    i--;
+                }while(i!=0);
+
             }
             else
             {
