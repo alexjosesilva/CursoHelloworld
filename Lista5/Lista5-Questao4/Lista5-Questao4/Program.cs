ec7a0ec557f9a6ce304b312ece0b8ed99ea2dca1
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-24 13:19:52 -0300

    Lista 5 Questao 4 criterio de parada

---
+++ Lista5/Lista5-Questao4/Lista5-Questao4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 4) Ler diversos números reais e exibir a quantidade e o somatório total dos números digitados. 
+// O valor -1 é o código de fim de entrada. 
+namespace Lista5_Questao4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtd=0;
+            float soma=0;
+            float numero;
+
+            do{
+                Console.Write("\n ");
+                numero = float.Parse(Console.ReadLine());
+                if (numero!=-1)
+                {
+                    soma += numero;
+                    qtd++;     
+                }
+               
+                
+            } while (numero != -1);
+
+            Console.Write("\n Quantidade: {0} Soma:{1} ",qtd,soma);
+            Console.ReadKey();
+
+        }
+    }
+}
