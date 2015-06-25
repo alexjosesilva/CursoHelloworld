e9c30f0dcf274fe3eab1b0a8bed0752570fbc7da
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-25 10:23:53 -0300

    Lista 3 Questao 9

---
+++ Lista3/Lista3-Questao9/Lista3-Questao9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Questao 9: Calcular a area do Trapezio
+ */
+namespace Lista3_Questao9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double altura, baseMaior, baseMenor,A;
+
+            altura    = Convert.ToDouble(Console.ReadLine());
+            baseMaior = Convert.ToDouble(Console.ReadLine());
+            baseMenor = Convert.ToDouble(Console.ReadLine());
+
+            A = ((baseMenor + baseMaior) * altura) / 2;
+
+            Console.Out.WriteLine("Area do Trapezio: " + A);
+            Console.ReadKey();
+        }
+    }
+}
