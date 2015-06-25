c43b5d3c167e265f45cebfed1b60bd2e6696eeb9
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-25 11:02:04 -0300

    Lista3 Questao 18: conversao de temperatura

---
+++ Lista3/Lista3-Questao18/Lista3-Questao18/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3_Questao18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double temperatura;
+
+            Console.Write("\n Inserir a temperatura Celsius: ");
+            temperatura = double.Parse(Console.ReadLine());
+
+            //calculo
+            temperatura = 180 * (temperatura + 32) / 100;
+
+            //exibir o resultado
+            Console.Write("\n Tempereatura F: {0}", temperatura);
+            Console.ReadKey();
+
+
+        }
+    }
+}
