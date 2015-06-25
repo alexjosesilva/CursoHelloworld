26a5ac7b2ada472c2546347d8eed474ad0290ae9
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-24 23:20:31 -0300

    nova lista

---
+++ Lista1/Lista1-Questao1/Lista1-Questao1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1_Questao1
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine("Resultado: "+(10+3));
+            Console.ReadKey();    
+        }
+    }
+}           
