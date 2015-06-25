96115a98fbbfcab218bbb92c34d3db99bca05d56
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-25 00:24:50 -0300

    Lista 2 Questao 7

---
+++ Lista2/Lista2-Questao7/Lista2-Questao7/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2_Questao7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1, numero2, numero3;
+
+            //obtendo os lados do triangulo
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            numero3 = Convert.ToInt32(Console.ReadLine());
+
+            //validar os lados do triangulo
+            //cada lado menor que a soma dos outros lados
+            if (numero1 < (numero2 + numero3))
+            {
+
+                if (numero2 < (numero1 + numero3))
+                {
+
+                    if (numero3 < (numero1 + numero2))
+                    {
+                       //tipo de triangulos
+
+                        if((numero1 == numero2) && (numero1==numero3) && (numero2==numero3))
+                            Console.Out.WriteLine("Triangulo Equilatero");
+                        else
+                            if((numero1 == numero2) || (numero1==numero3) || (numero2==numero3))
+                                Console.Out.WriteLine("Triangulo Isosceles");
+                            else
+                                Console.Out.WriteLine("Triangulo Equilatero");
+                    }
+                    else{
+                        Console.Out.WriteLine("Lados não formam um triangiulo!");
+                    }
+                }
+                else{
+                    Console.Out.WriteLine("Lados não formam um triangiulo!");
+                }
+            }
+            else{
+                Console.Out.WriteLine("Lados não formam um triangiulo!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
