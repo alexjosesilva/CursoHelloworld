5237651393311f792cb9339ebf6c14ac370f0414
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-25 00:08:17 -0300

    Lista2 Questao 1

---
+++ Lista2/Lista2-Questao1/Lista2-Questao1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2_Questao1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero == 0)
+                Console.Out.WriteLine("Saida: " + 0);
+            else
+                if (numero < 0)
+                    Console.Out.WriteLine("Saida Menor q Zero");
+                else
+                    Console.Out.WriteLine("Saida Maior q Zero");
+            Console.ReadKey();
+        }
+    }
+}
