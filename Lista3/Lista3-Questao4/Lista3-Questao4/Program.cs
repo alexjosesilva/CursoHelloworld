2a2edbe0401753c3578257b0f336c22b1fb4298b
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-25 10:13:43 -0300

    Lista3 Questao 4

---
+++ Lista3/Lista3-Questao4/Lista3-Questao4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3_Questao4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pnota, snota, media;
+
+            pnota = Convert.ToDouble(Console.ReadLine());
+            snota = Convert.ToDouble(Console.ReadLine());
+
+            media = (pnota * 2 + snota * 3) / 2;
+
+            Console.Out.WriteLine("media: " + media);
+            Console.ReadKey();
+
+        }
+    }
+}
