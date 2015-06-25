f07a73d8a2722a3e7703d1fa572eebb4a25fa783
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-25 10:12:13 -0300

    Lista 3 Questao 3

---
+++ Lista3/Lista3-Questao3/Lista3-Questao3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3_Questao3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero1, numero2;
+
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            numero2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Out.WriteLine("Resultado da divisao: "+(numero1/numero2));
+
+            Console.ReadKey();
+        }
+    }
+}
