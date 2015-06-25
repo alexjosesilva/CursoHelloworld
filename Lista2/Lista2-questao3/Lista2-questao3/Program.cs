6dce72e55e821d6992b0f56528f6e6a6a4e88aa5
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-25 00:12:38 -0300

    Lista 2 Questao 3

---
+++ Lista2/Lista2-questao3/Lista2-questao3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2_questao3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if(numero % 2==0)
+                Console.Out.WriteLine("Numero Par");
+            else
+                Console.Out.WriteLine("Numero Impar");
+            Console.ReadKey();
+
+        }
+    }
+}
