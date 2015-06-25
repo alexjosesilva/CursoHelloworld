97851a2dc120ff284fe495c88fd5f41630bd862d
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-25 10:22:46 -0300

    Lista 3 Questao 8

---
+++ Lista3/Lista3-Questao8/Lista3-Questao8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * Questao 8: Fazer um programa que leia um peso em Kg e calcule e mostra em gramas
+ * 
+ */
+namespace Lista3_Questao8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double peso;
+
+            peso = Convert.ToDouble(Console.ReadLine());
+            Console.Out.WriteLine("Peso em gramas: " + peso * 1000);
+            Console.ReadKey();
+        }
+    }
+}
