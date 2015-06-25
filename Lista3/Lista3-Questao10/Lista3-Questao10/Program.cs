47bc27389f5abf1ef2aa89b86e18909e42737ed8
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-25 10:24:47 -0300

    Lista  3 Questao 10

---
+++ Lista3/Lista3-Questao10/Lista3-Questao10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Questao 10: Calcular a area do Quadrado
+ */
+namespace Lista3_Questao10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, lado;
+
+            lado = Convert.ToDouble(Console.ReadLine());
+            A = lado * lado;
+            Console.WriteLine("Area: " + A);
+            Console.ReadKey();
+        }
+    }
+}
