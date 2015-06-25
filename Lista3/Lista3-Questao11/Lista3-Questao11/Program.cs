2bf0a731ed768f6ca9e04bda5a654115fdae856b
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-25 10:26:13 -0300

    Lista3 Questao 11

---
+++ Lista3/Lista3-Questao11/Lista3-Questao11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * Questao 11: Calcular area do losangulo 
+ */
+namespace Lista3_Questao11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double diaMaior, diaMenor,A;
+
+            diaMenor = Convert.ToDouble(Console.ReadLine());
+            diaMaior = Convert.ToDouble(Console.ReadLine());
+
+            A = (diaMaior*diaMenor)/2;
+            Console.WriteLine("Area: " + A);
+            Console.ReadKey();
+        }
+    }
+}
