df762b7172679b822a9ee86123b2187c8da5f2fc
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-24 23:21:46 -0300

    lista 1 questao 2

---
+++ Lista1/Lista1-Questao2/Lista1-Questao-2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1_Questao_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine("Resultado Produto: " + (3 * 29));
+            Console.ReadKey();  
+        }
+    }
+}
