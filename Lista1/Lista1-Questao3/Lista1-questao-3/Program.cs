d9ad6b6a16d25b1e84d8ac284521cea9453fccda
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-24 23:51:06 -0300

    Lista1 questao 3

---
+++ Lista1/Lista1-Questao3/Lista1-questao-3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1_questao_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine("Resultado Produto: " + (129 % 5));
+            Console.ReadKey();  
+        }
+    }
+}
