e10737035e67e9ba6aa0a80047a7d005fb9ff19c
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-25 00:11:52 -0300

    Lista 2 Questao 2

---
+++ Lista2/Lista2-Questao2/Lista2-Questao2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2_Questao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1, numero2;
+
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            numero2 = Convert.ToInt32(Console.ReadLine());
+
+            if (numero1 > numero2)
+                Console.Out.WriteLine("Numero Maior: "+numero1);
+            else
+                Console.Out.WriteLine("Numero Maior: " + numero2);
+            
+            Console.ReadKey();
+        }
+    }
+}
