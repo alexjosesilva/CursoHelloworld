60266aebc7564e18b3dc92f36c8f24baccb682b6
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-25 10:01:15 -0300

    Lista3-Questao1

---
+++ Lista3/Lista3-Questao1/Lista3-Questao1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3_Questao1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1, numero2;
+            
+            Console.Out.Write("Primeiro numero: ");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.Out.Write("Segundo numero: ");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Out.WriteLine("Resultado: "+(numero1-numero2));
+            Console.ReadKey();
+        }
+    }
+}
