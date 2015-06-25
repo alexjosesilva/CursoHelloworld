81672272e5d1a2afc3dfe1e5be8d92b4ae86b86c
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-25 10:27:04 -0300

    Lista 3 Questao 12

---
+++ Lista3/Lista3-Questao12/Lista3-Questao12/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ *  Questao 12: Pedir o valor do Salario Minimo  e o Valor do Salario de um funcionario. O programa
+ *  dever informar quantos salarios minimos um funcionario .
+ */
+namespace Lista3_Questao12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //variaveis:::::::
+            double salarioM, salarioF, resultado;
+
+            //entrada::::::::
+            Console.Write("Salario Minimo: ");
+            salarioM = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Salario Salario: ");
+            salarioF = Convert.ToDouble(Console.ReadLine());
+
+            //calculo::::::::
+            resultado = salarioF / salarioM;
+
+            //saida::::::::::
+            Console.WriteLine("REsultado: "+ Math.Round(resultado,2) );
+            Console.ReadKey();
+                
+
+        }
+    }
+}
