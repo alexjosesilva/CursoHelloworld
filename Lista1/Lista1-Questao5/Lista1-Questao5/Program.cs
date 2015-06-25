73abf4884f034817163c00c2f158945d8dcffe58
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-24 23:54:22 -0300

    Lista1 Questao 5

---
+++ Lista1/Lista1-Questao5/Lista1-Questao5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1_Questao5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+
+            nome = Console.ReadLine();
+            Console.Out.WriteLine("Seja bem vindo: " + nome);
+            Console.ReadKey();
+        }
+    }
+}
