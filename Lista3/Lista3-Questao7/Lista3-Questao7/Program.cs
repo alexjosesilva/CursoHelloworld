8df09452787fafcaf15a239c56b47209acf085d8
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-25 10:21:34 -0300

    Lista 3 Questao 7

---
+++ Lista3/Lista3-Questao7/Lista3-Questao7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * Questão 7: Fazer um programa  que receba o peso de uma pessoa, calcule e mostre:
+ * a. O novo peso se a pessoa engordar 15%  sobre o peso digitado
+ * b. O novo peso se a pessoa emagrecer 20%  sobre o peso digitado
+ */
+
+
+namespace Lista3_Questao7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double peso;
+
+            Console.Write("Informe o peso: ");
+            peso = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Se engordar: "+(peso+(peso*0.15)));
+            Console.WriteLine("Se emagrecer: "+(peso-(peso*0.20)));
+
+            Console.ReadKey();
+           
+        }
+    }
+}
