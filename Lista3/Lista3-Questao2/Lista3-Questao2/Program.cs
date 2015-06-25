3214dd24e87f1d228a98c154d22b522403940e58
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-25 10:10:43 -0300

    Lista 3 Questao 2

---
+++ Lista3/Lista3-Questao2/Lista3-Questao2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3_Questao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variaveis....
+            int numero1, numero2, numero3;
+            double produto;
+
+            //Entrada de Saida....
+            Console.Out.Write("Numero 1: ");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Out.Write("Numero 2: ");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Out.Write("Numero 3: ");
+            numero3 = Convert.ToInt32(Console.ReadLine());
+
+            //multiplicação
+            produto = numero1 * numero2 * numero3;
+            Console.Out.WriteLine("Produto: " + produto);
+            Console.ReadKey();
+
+        }
+    }
+}
