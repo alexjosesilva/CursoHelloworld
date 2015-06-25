4b8e3dc0183ded3dff03afec8e216b598e0b31d8
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-25 00:25:28 -0300

    Lista 2 Questao 8

---
+++ Lista2/Lista2-Questao8/Lista2-Questao8/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2_Questao8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variaveis do sistema
+            double peso, imc,altura;
+            int idade;
+            
+            //obtenção de dados
+            Console.Out.Write("Informar o peso(KG): ");
+            peso = Convert.ToDouble(Console.ReadLine());
+
+            Console.Out.Write("Informar a altura(M): ");
+            altura = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Out.Write("Informar a idade(anos): ");
+            idade = Convert.ToInt32(Console.ReadLine());
+           
+
+            //calcudo do IMC
+            imc = peso / Math.Pow(altura,2);
+
+            if (imc < 18.5)
+                Console.Out.WriteLine("Abaixo do Peso");
+            else
+                if(imc >= 18.5 && imc<25)
+                    Console.Out.WriteLine("Peso Normal");
+            else
+                if(imc>=25 && imc<30)
+                    Console.Out.WriteLine("Excesso Peso");
+            else
+                if(imc>=30 && imc<35)
+                    Console.Out.WriteLine("Obesidade 1");
+            else
+                if(imc>=35 && imc<40)
+                    Console.Out.WriteLine("Obesidade 2");
+           else
+                if(imc>=40)
+                    Console.Out.WriteLine("Obesidade 3");
+            
+            Console.ReadKey();
+
+        }
+    }
+}
