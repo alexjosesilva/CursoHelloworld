3285c75b920723a52504784fc0415f209349b9d5
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-25 00:07:02 -0300

    Lista1 Questao 9

---
+++ Lista1/Lista1-Questao9/Lista1-Questao9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1_Questao9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota1, nota2, nota3, nota4;
+
+            nota1 = Convert.ToInt32(Console.ReadLine());
+            nota2 = Convert.ToInt32(Console.ReadLine());
+            nota3 = Convert.ToInt32(Console.ReadLine());
+            nota4 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Out.WriteLine("Media: "+( (nota1+nota2+nota3+nota4)/4) );
+            Console.ReadKey();
+
+
+        }
+    }
+}
