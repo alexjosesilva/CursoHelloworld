26febe103d41cf8948dd31957d0da62cdb3dc17c
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-24 23:56:56 -0300

    Lista1 Questao 7

---
+++ Lista1/Lista1-Questao8/Lista1-Questao8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1_Questao8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1, numero2, numero3;
+
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            numero3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Out.WriteLine("(n1 + n2) / n3: "+( (numero1+numero2)/numero3 ));
+            Console.ReadKey();
+        }
+    }
+}
