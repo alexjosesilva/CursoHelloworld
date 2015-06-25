ec239bf6c6f44b902bfb49ffb5f586ebbac49fb0
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-24 23:54:56 -0300

    Lista1 Questao 6

---
+++ Lista1/Lista1-Questao6/Lista1-Questao6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1_Questao6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.Out.WriteLine("Resultado: " +(numero * 2));
+            Console.ReadKey();
+        }
+    }
+}
