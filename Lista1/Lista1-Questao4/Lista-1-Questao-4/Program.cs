7f701c115b0d60be338406d8d22b6d5b3dadca53
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-24 23:52:32 -0300

    Lista1: Questao 4

---
+++ Lista1/Lista1-Questao4/Lista-1-Questao-4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_1_Questao_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1;
+
+            numero1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Out.WriteLine("Numero: " + numero1);
+            Console.ReadKey();
+        }
+    }
+}
