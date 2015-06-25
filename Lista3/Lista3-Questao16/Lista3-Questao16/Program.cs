f0cb62b658280c071df4f618abf09e4d9a3f6c08
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-25 09:47:06 -0300

    Lista3 Questao 16 e 17.....

---
+++ Lista3/Lista3-Questao16/Lista3-Questao16/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3_Questao16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double catetoA, catetoB, hipotenusa;
+
+            Console.Write("CatetoA: ");
+            catetoA = double.Parse(Console.ReadLine());
+
+            Console.Write("CatetoB: ");
+            catetoB = double.Parse(Console.ReadLine());
+
+            hipotenusa = Math.Sqrt((Math.Pow(catetoA, 2) + Math.Pow(catetoB, 2)));
+
+            Console.WriteLine("Hipotenusa: " + Math.Round(hipotenusa, 2));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lista3/Lista3-Questao17/Lista3-Questao17/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3_Questao17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double R;
+            double pi = 3.14;
+
+            R = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Area do Circulo: " + (2 * pi));
+            Console.WriteLine("Area do Circulo: " + (pi * (Math.Pow(R, 2))));
+            Console.WriteLine("Volume do Circulo: " + (3 * (pi * (Math.Pow(R, 3)))) / 4);
+
+            Console.ReadKey();
+        }
+    }
+}
