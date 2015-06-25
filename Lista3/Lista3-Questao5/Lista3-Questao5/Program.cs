5c162b49ed2f4d7b280f932d6ab3e3de3d7e71e5
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-25 10:19:48 -0300

    Lista 3 Questao 5

---
+++ Lista3/Lista3-Questao5/Lista3-Questao5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * Questão 5: Fazer um programa que receba o preço  de um produto, calcule e mostre
+ * o novo preço sabendo-se que este sofreu um desconto de 10%
+ */
+
+
+namespace Lista3_Questao5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco;
+
+            preco = Convert.ToDouble(Console.ReadLine());
+            preco = preco - (preco * 0.10);
+
+            Console.Out.WriteLine("Novo Preco: " + preco);
+            Console.ReadKey();
+        }
+    }
+}
