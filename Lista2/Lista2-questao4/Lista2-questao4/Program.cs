89e1eb962b643865be56d059b69649a836991b4c
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-25 00:16:20 -0300

    Lista2 Questao 4

---
+++ Lista2/Lista2-questao4/Lista2-questao4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2_questao4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero > 0)
+            {
+                if (numero % 2 == 0)
+                    Console.Out.WriteLine("Numero Positivo e Par!");
+                else
+                    Console.Out.WriteLine("Numero Positivo e Impar!");
+            }
+            else
+            {
+                if (numero % 2 == 0)
+                    Console.Out.WriteLine("Numero Negativo e Par!");
+                else
+                    Console.Out.WriteLine("Numero Negativo e Impar!");
+
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
