f59d8b3d1e731854027d2d132b0e6b97de000f91
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-26 02:48:40 -0300

    Lista 2 Questao 22

---
+++ Lista3/Lista3-Questao22/Lista3-Questao22/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//numero de lados de um poligono
+
+namespace Lista3_Questao22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+
+            N = int.Parse(Console.ReadLine());
+
+            N = N * (N - 3) / 2;
+
+            Console.Write("\n Numero Diagonais: {0}",N);
+            Console.ReadKey();
+        }
+    }
+}
