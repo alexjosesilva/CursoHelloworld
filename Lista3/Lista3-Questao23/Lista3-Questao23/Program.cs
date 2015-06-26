d6b8a303b377fe73745bd0bdf34c27d98c96d24b
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-26 03:48:41 -0300

    Lista 3 Questao 23

---
+++ Lista3/Lista3-Questao23/Lista3-Questao23/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Soma dos Angulos internos de um triangulo
+
+namespace Lista3_Questao23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double angulo1, angulo2,angulo3;
+
+            angulo1 = double.Parse(Console.ReadLine());
+            angulo2 = double.Parse(Console.ReadLine());
+
+            angulo3 = 180 - (angulo1 + angulo2);
+
+            Console.Write("\n Angulo 3: {0}", angulo3);
+            Console.ReadKey();
+            
+        }
+    }
+}
