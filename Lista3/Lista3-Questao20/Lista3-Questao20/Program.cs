ea9113a8d18043ab8470124a3d5e677652870a90
Alex José Silva <alexjosesilvati@gmail.com>
2015-06-25 23:50:14 -0300

    Lista 3 Questao 20: calculo matematico, distancia

---
+++ Lista3/Lista3-Questao20/Lista3-Questao20/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3_Questao20
+{
+    class Program
+    {
+        const double pi = 3.14;
+
+        static void Main(string[] args)
+        {
+            double angulo, distancia, medida;
+
+            Console.Write("\n Angulo: ");
+            angulo = double.Parse(Console.ReadLine());
+            Console.Write("\n Medida: ");
+            medida = double.Parse(Console.ReadLine());
+
+            distancia = Math.Sin(angulo) * medida;
+
+            Console.Write("\n {0:F} M", Math.Abs(distancia));
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
