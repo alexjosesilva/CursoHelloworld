1033b1a78e6911dff5820079d0681983cf4e8587
Alex José Silva <alexjosesilvati@gmail.com>
2015-07-02 18:12:08 -0300

    Lista 6 Questao 3: Menor Numero de dois...

---
+++ Lista6/Lista6Questao3/Lista6Questao3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Questao 3: Mostar Menor numero
+namespace Lista6Questao3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pnumero, snumero, maior;
+
+            pnumero = Int32.Parse(Console.ReadLine());
+            snumero = Int32.Parse(Console.ReadLine());
+
+            if (pnumero<snumero)
+            {
+                Console.Write("\n Menor: {0} ", pnumero);
+            }
+            else
+            {
+                Console.Write("\n Menor: {0} ", snumero);
+            }
+           
+            Console.ReadKey();
+        }
+    }
+}
