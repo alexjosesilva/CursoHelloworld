78f10614b05e9d605c1bc2c04e10defff4e530a1
Alex José Silva <alexjosesilvati@gmail.com>
2015-07-02 18:24:42 -0300

    LIsta 6 Questao 4: maior de tres numeros

---
+++ Lista6/Lista6Questao4/Lista6Questao4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista6Questao4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pnumero, snumero, tnumero; 
+            
+
+            pnumero = Int32.Parse(Console.ReadLine());
+            snumero = Int32.Parse(Console.ReadLine());
+            tnumero = Int32.Parse(Console.ReadLine());
+
+            if (pnumero > snumero && pnumero>tnumero)
+            {
+                Console.Write("\n Maior: {0} ", pnumero);
+            }
+            else if (snumero > tnumero)
+            {
+                 Console.Write("\n Maior: {0} ", snumero);
+            }
+            else
+            {
+                Console.Write("\n Maior: {0} ", tnumero);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
