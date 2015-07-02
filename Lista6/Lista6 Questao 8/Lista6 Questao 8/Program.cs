4b89da4204dc4881bd84d85c84eb8a98959ffddf
Alex José Silva <alexjosesilvati@gmail.com>
2015-07-02 19:40:49 -0300

    Lista 6 Questao 8: reajuste de valores, calculo percentual

---
+++ Lista6/Lista6 Questao 8/Lista6 Questao 8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista6_Questao_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario;
+
+            Console.WriteLine("Informe o salario: ");
+            salario = double.Parse(Console.ReadLine());
+
+
+            if (salario <= 300)
+            {
+                Console.WriteLine("Salario Reajustado: {0}",300+(300*0.35));
+            }
+            else if(salario > 300)
+            {
+                 Console.WriteLine("Salario Reajustado: {0}",300+(300*0.15));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
