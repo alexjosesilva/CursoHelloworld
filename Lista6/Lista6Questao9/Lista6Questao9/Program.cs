ee185f26c9d408569795257bfd419daf27d88e71
Alex José Silva <alexjosesilvati@gmail.com>
2015-07-02 19:56:07 -0300

    Lista 6 Questao 9: calculos matematicos!

---
+++ Lista6/Lista6Questao9/Lista6Questao9/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista6Questao9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variaveis 
+            double saldoMedio;
+
+            //leitura 
+            Console.WriteLine("Informe o saldo Medio : ");
+            saldoMedio = double.Parse(Console.ReadLine());
+
+            //condicional
+            if (saldoMedio > 400)
+            {
+                Console.WriteLine("Saldo Medio Reajustado: {0}", saldoMedio + (saldoMedio * 0.30));
+            }
+            else if (saldoMedio <= 400 && saldoMedio > 300 )
+            {
+                Console.WriteLine("Saldo Medio Reajustado: {0}", saldoMedio + (saldoMedio * 0.25));
+            }
+            else if (saldoMedio <= 300 && saldoMedio > 200)
+            {
+                Console.WriteLine("Saldo Medio Reajustado: {0}", saldoMedio + (saldoMedio * 0.20));
+            }
+            else if (saldoMedio <= 200)
+            {
+                Console.WriteLine("Saldo Medio Reajustado: {0}", saldoMedio + (saldoMedio * 0.10));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
