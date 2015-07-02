1e16f22468f3995d16e4330e31b8b8242b27146e
Alex José Silva <alexjosesilvati@gmail.com>
2015-07-02 19:45:58 -0300

    Lista 6 Questao 6: calculo de porcentagem

---
+++ Lista6/Lista6Questao6/Lista6Questao6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista6Questao6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario;
+
+            Console.WriteLine("Informe o salario: ");
+            salario = double.Parse(Console.ReadLine());
+
+
+            if (salario <= 500)
+            {
+                Console.WriteLine("Salario Reajustado: {0:C}", salario + (salario * 0.30));
+            }
+            else if (salario > 500)
+            {
+                Console.WriteLine("Salario Não Reajustado!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
