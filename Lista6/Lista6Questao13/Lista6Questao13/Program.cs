8f9fb600464405f3f9707c237a21690fd36e0a5c
Alex José Silva <alexjosesilvati@gmail.com>
2015-07-02 22:49:28 -0300

    Lista6 Questao 13

---
+++ Lista6/Lista6Questao13/Lista6Questao13/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista6Questao13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variaveis
+            double preco;
+
+            //ler dados
+            preco = double.Parse(Console.ReadLine());
+
+            //condicional
+            if (preco<=50)
+            {
+                preco += preco * 0.05;
+            }
+            else if(preco>50 && preco<=100)
+            {
+                preco += preco * 0.10;
+            }
+            else if (preco>100)
+            {
+                preco += preco * 15;
+            }
+
+
+            if (preco >= 80)
+            {
+                Console.WriteLine("Classificação: Barato Preço: {0:C} ",preco);
+            }
+            else if (preco > 80 && preco <= 120)
+            {
+                Console.WriteLine("Classificação: Normal Preço: {0:C} ", preco);
+            }
+            else if (preco > 120 && preco <= 200)
+            {
+                Console.WriteLine("Classificação: Caro Preço: {0:C} ", preco);
+            }
+            else if (preco > 200)
+            {
+                Console.WriteLine("Classificação: Muito Caro Preço: {0:C} ", preco);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
