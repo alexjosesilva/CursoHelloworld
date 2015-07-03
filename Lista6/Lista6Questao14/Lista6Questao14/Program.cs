4b9315ae087367cab0ab6de5f0ca8dfee949ce32
Alex José Silva <alexjosesilvati@gmail.com>
2015-07-02 22:57:24 -0300

    Lista 6 Questao 13: reajuste de salario

---
+++ Lista6/Lista6Questao14/Lista6Questao14/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista6Questao14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variavel
+            double salario;
+
+            //leitura
+            Console.WriteLine("Informe o salario: ");
+            salario = double.Parse(Console.ReadLine());
+
+            //calculos
+            if (salario <= 300)
+            {
+                Console.WriteLine("Salario Reajustado: {0:C}", salario + (salario * 0.5));
+            }
+            else if (salario > 300 && salario <= 500)
+            {
+                Console.WriteLine("Salario Reajustado: {0:C}", salario + (salario * 0.4));
+            }
+            else if (salario > 500 && salario <= 700)
+            {
+                Console.WriteLine("Salario Reajustado: {0:C}", salario + (salario * 0.3));
+            }
+            else if (salario > 700 && salario <= 800)
+            {
+                Console.WriteLine("Salario Reajustado: {0:C}", salario + (salario * 0.2));
+            }
+            else if (salario > 800 && salario <= 1000)
+            {
+                Console.WriteLine("Salario Reajustado: {0:C}", salario + (salario * 0.1));
+            }
+            else
+            {
+                Console.WriteLine("Salario Reajustado: {0:C}", salario + (salario * 0.5));
+            }
+            Console.ReadLine();
+        }
+    }
+}
