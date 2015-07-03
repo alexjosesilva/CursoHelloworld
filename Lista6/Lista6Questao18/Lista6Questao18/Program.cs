e6d3f1bc00eb90861f81f2b5eca31d2ed0913de5
Alex José Silva <alexjosesilvati@gmail.com>
2015-07-02 23:43:37 -0300

    Lista 6 Questao 18

---
+++ Lista6/Lista6Questao18/Lista6Questao18/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista6Questao18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+
+            Console.WriteLine("Informe a senha: ");
+            idade = int.Parse(Console.ReadLine());
+
+            if (idade >= 18)
+            {
+                Console.WriteLine("Maior  de Idade!");
+            }
+            else
+            {
+                Console.WriteLine("Menor de Idade!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
