dcd11cc412d77e7ddb65c6f909564370711c4d87
Alex José Silva <alexjosesilvati@gmail.com>
2015-07-02 22:19:09 -0300

    Lista 6 Questao 12: calculos

---
+++ Lista6/Lista6Questao12/Lista6Questao12/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista6Questao12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variavel
+            double salario;
+
+            //leitura
+            Console.WriteLine("Informe o salario: ");
+            salario = double.Parse(Console.ReadLine());
+
+            //calculos
+            if (salario <= 350)
+            {
+                Console.WriteLine("Salario Reajustado: {0:C}", (salario + 100) - (salario * 0.07));
+            }
+            else if (salario > 350 && salario <= 600)
+            {
+                Console.WriteLine("Salario Reajustado: {0:C}", (salario + 75) - (salario * 0.07));
+            }
+            else if (salario > 600 && salario <= 900)
+            {
+                Console.WriteLine("Salario Reajustado: {0:C}", (salario + 50) - (salario * 0.07));
+            }
+            else if (salario>900)
+            {
+                Console.WriteLine("Salario Reajustado: {0:C}", (salario + 35) - (salario * 0.07));
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
