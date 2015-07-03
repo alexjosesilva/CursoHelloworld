0125814d8381e68d9143aba926e30b13a73df8ca
Alex José Silva <alexjosesilvati@gmail.com>
2015-07-03 01:11:28 -0300

    Lista 6 Questao 22

---
+++ Lista6/Lista6Questao22/Lista6Questao22/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista6Questao22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variaveis....
+            int idade;
+            double peso;
+
+            //leitura....
+            Console.WriteLine("inserir idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("inserir peso: ");
+            peso = double.Parse(Console.ReadLine());
+
+            if (idade<=20)
+            {
+                if (peso<=60)
+                {
+                    Console.WriteLine("Grupo 9");
+                }
+                else if (peso>60 && peso <=90)
+                {
+                    Console.WriteLine("Grupo 8");
+                }
+                else if (peso > 90)
+                {
+                    Console.WriteLine("Grupo 7");
+                }
+            }
+            else if (idade>20 && idade <=50)
+            {
+                if (peso <= 60)
+                {
+                    Console.WriteLine("Grupo 6");
+                }
+                else if (peso > 60 && peso <= 90)
+                {
+                    Console.WriteLine("Grupo 5");
+                }
+                else if (peso > 90)
+                {
+                    Console.WriteLine("Grupo 4");
+                }
+            }
+            else if (idade>50)
+            {
+                if (peso <= 60)
+                {
+                    Console.WriteLine("Grupo 3");
+                }
+                else if (peso > 60 && peso <= 90)
+                {
+                    Console.WriteLine("Grupo 2");
+                }
+                else if (peso > 90)
+                {
+                    Console.WriteLine("Grupo 1");
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
