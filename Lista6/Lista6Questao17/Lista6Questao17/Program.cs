4532f60f69207456d5e2ae7cb52430a8c1c5057f
Alex José Silva <alexjosesilvati@gmail.com>
2015-07-02 23:34:27 -0300

    Lista 6 Questao 17: validade de senha

---
+++ Lista6/Lista6Questao17/Lista6Questao17/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista6Questao17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha;
+
+            Console.WriteLine("Informe a senha: ");
+            senha = int.Parse(Console.ReadLine());
+
+            if (senha==4531)
+            {
+                Console.WriteLine("Permissao de Acesso!");
+            }
+            else
+            {
+                Console.WriteLine("Permissao Negada!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
