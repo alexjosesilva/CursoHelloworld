0052908e6d0c7cc62e68a3a31d6260f614ca4feb
Alex José Silva <alexjosesilvati@gmail.com>
2015-07-02 21:55:28 -0300

    Lista 6 Questao 11: calculos numericos

---
+++ Lista6/Lista6Questao11/Lista6Questao11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista6Questao11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario;
+
+            Console.WriteLine("Informe o salario: ");
+            salario = double.Parse(Console.ReadLine());
+
+
+            if (salario <= 300)
+            {
+                Console.WriteLine("Salario Reajustado: {0:C}", salario + (salario * 0.15));
+            }
+            else if (salario > 300 && salario <= 600)
+            {
+                Console.WriteLine("Salario Reajustado: {0:C}", salario + (salario * 0.10));
+            }
+            else if(salario >600 && salario <= 900)
+            {
+                Console.WriteLine("Salario Reajustado: {0:C}", salario + (salario * 0.05));
+            }
+            Console.ReadLine();
+        }
+    }
+}
