f961d4c418b23009df55304228ddbd7b0f201479
Alex José Silva <alexjosesilvati@gmail.com>
2015-07-03 00:21:10 -0300

    Lista 6 Questao 20

---
+++ Lista6/Lista6Questao20/Lista6Questao20/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista6Questao20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+
+            idade = int.Parse(Console.ReadLine());
+
+            if (idade>=5 && idade<=7)
+            {
+                Console.WriteLine("Infantil");
+            }
+            else if (idade>=8 && idade<=10)
+            {
+                Console.WriteLine("Juvenil");
+            }
+            else if (idade>=11 && idade<=15)
+            {
+                Console.WriteLine("Adolescente");
+            }
+            else if (idade>=16 && idade<=30)
+            {
+                Console.WriteLine("Adulto");
+            }
+            else if (idade > 30 )
+            {
+                Console.WriteLine("Senior");
+            }
+           
+        }
+    }
+}
